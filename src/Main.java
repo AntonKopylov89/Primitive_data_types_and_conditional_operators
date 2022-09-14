e6ef05d9950817217a025f6da59f414c59f4943f
Anton_Kopylov <102693281+AntonKopylov89@users.noreply.github.com>
2022-09-14 04:33:59 -0700

    Update Main.java
    Add comment

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         int account = 300;
         int accountReplenishment = 1500;
-        int bonus = (accountReplenishment > 1000) ? (bonus = accountReplenishment / 100) : (bonus = 0); // тернарный оператор
+        int bonus = (accountReplenishment > 1000) ? (bonus = accountReplenishment / 100) : (bonus = 0); // тернарный оператор вместо if (accountReplenishment > 1000) { bonus = accountReplenishment / 100; } else { bonus = 0; }  
         int totalAccount = account + accountReplenishment + bonus;
 
         System.out.println("Итоговый счет: " + totalAccount);
